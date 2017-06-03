425c7167139801897fd5aa2725be13405f97cf68
abhi-narwal <anarwal@learnvest.com>
2017-06-03 19:11:50 -0400

    Adding docker-compose for influxdb and grafana

--- src/main/java/com/example/GreetingController.java
+++ src/main/java/com/example/GreetingController.java
@@ -5,12 +5,7 @@ import kamon.annotation.EnableKamon;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
-
-/**
- * Created by anarwal on 6/2/17.
- */
 
 @RestController
 @EnableKamon
